0ad0fb96c3847062f4fb892253bddb502166f082
tascord <35181375+tascord@users.noreply.github.com>
2018-09-24 18:41:07 +1000

    Did some things
    New Things ----------
    - Reworked Database system
    - Updated help command
    - Fixed kick and ban text
    - Started work on welcome message
    
    Things I need to fix ------
    - Errors recalling Database info
    - Web based things

--- okaygoogle.js
+++ okaygoogle.js
@@ -15,9 +15,12 @@ moment().tz("Australia/Melbourne").format();
 
 //Variables
 var googleColours = ['0x4681f3', '0xdf2f35', '0xf6c400', '0x477ff5', '0x36bc50', '0xdf2f35'];
-var chatCommands = [''];
-var musicCommands = ["**Play** `[Url / Term]`\n Adds The URL / Search Term To The Queue", "**Skip** `[number]`\n  Skips One Song (or the specified ammount to skip)", "**Queue**\n Shows The Queue", "**Pause / Resume**\n Pauses Or Resumes Music Playback", "**Volume** `[Level]`\n Sets Playback Volume (0 - 200)", "**Leave**\n Removes The Bot From The Voice Channel", "**Clearqueue**\n Clears The Queue"];
-var adminCommands = [''];
+var chatCommands = ['**Coin**\n Flip a coin!', '**Echo**\n Copy whatever you say!'];
+var musicCommands = ["**Play** `[Url / Term]`\n Adds The URL / Search Term To The Queue", "**Skip** `[Number]`\n Skips One Song (or the specified ammount to skip)", "**Queue**\n Shows The Queue", "**Pause / Resume**\n Pauses Or Resumes Music Playback", "**Volume** `[Level]`\n Sets Playback Volume (0 - 200)", "**Leave**\n Removes The Bot From The Voice Channel", "**Clearqueue**\n Clears The Queue"];
+var adminCommands = ['**Setup**\n Setup the servers settings. These can be modified after setup', '**Ban / Kick** `[User] [Reason]`\n Punishes the given user for the given reason', '**Ci** `[Value]`\n Clears the amount of messages you specify'];
+
+//Login the bot
+client.login(config.token);
 
 //Console Stylizing
 function cError (parseError) {
@@ -28,8 +31,8 @@ function cWarn (parseWarn) {
   console.log(chalk.bgYellow.bold('Warn') + ' ' + parseWarn);
 }
 
-function cPass (parsePass) {
-  console.log(chalk.bgGreen.bold('Pass') + ' ' + parsePass);
+function cInfo (parseInfo) {
+  console.log(chalk.bgGreen.bold('Info') + ' ' + parseInfo);
 }
 
 function uError (uParseError) {
@@ -37,45 +40,22 @@ function uError (uParseError) {
 }
 
 //Startup Database
-const sequelize = new Sequelize('database', 'Username', 'Password', {
+const sequelize = new Sequelize('serverDatabase', 'Username', 'Password', {
     host: 'localhost',
     dialect: 'sqlite',
     logging: false,
-    storage: 'googleDB.sqlite',
+    storage: 'serverData.sqlite',
 });
 
-const UserDataPS = sequelize.define('tags', {
-    serverid: {
+const serverData = sequelize.define('serverDataTable', {
+    serverid:{
         type: Sequelize.INTEGER,
-        defaultValue: 0,
-        allowNull: false,
+        unique: true,
     },
-    userid: {
-        type: Sequelize.INTEGER,
-        defaultValue: 0,
-        allowNull: false,
-    },
-    warns: {
-        type: Sequelize.INTEGER,
-        defaultValue: 0,
-        allowNull: false,
-    }
-});
-
-const ServerData = sequelize.define('tags', {
-    serverid: {
-        type: Sequelize.INTEGER,
-        defaultValue: 0,
-        allowNull: false,
-    },
-    ownerid: {
-        type: Sequelize.INTEGER,
-        defaultValue: 0,
-        allowNull: false,
-    },
+    ownerid: Sequelize.INTEGER,
     modlog: Sequelize.TEXT,
     servername: Sequelize.TEXT,
-    swearfilter: Sequelize.TEXT
+    welcomemessage: Sequelize.TEXT
 });
 
 //Musicbot
@@ -93,20 +73,32 @@ Music.start(client, {
 //On Bot Startup
 client.once('ready', () => {
     console.log(`${chalk.cyan(`\nOkay, `)}${chalk.cyan.bold(`${client.user.tag}!\n`)}`);
-    UserDataPS.sync();
-    ServerData.sync();
+    serverData.sync();
 });
 
-//Login
-client.login(config.token);
+//When user joins guild send them welcome message if avaliable
+client.on("guildMemberAdd", async member => {
 
+  try {
+    const guildWelcome = await serverData.findOne({ where: { serverid: message.member.guild.id } });
+  } catch (e) { }
+
+});
+
+//Commands
 client.on('message', async message => {
+
+if(!message.member.hasPermission("EMBED_LINKS") || !message.member.hasPermission("MANAGE_MESSAGES")) {
+if(message.content.toLowerCase().includes('www.') || message.content.toLowerCase().includes('http')) {
+  message.delete();
+  message.reply("Please no URLS in chat");
+}
+}
+
 //Get Database Info
 try {
-const userDataa = await ServerData.findOne({ where: { serverid: message.member.guild.id } });
-const serverDataa = await UserDataPS.findOne({ where: { serverid: message.member.guild.id } });
+const serverDataMessage = await serverData.findOne({ where: { serverid: message.member.guild.id } });
 } catch (e) { }
-
 
 //Variables & Constants
 var now = moment();
@@ -125,17 +117,8 @@ if (message.content.toLowerCase().indexOf(config.prefix) !== 0) return;
 const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
 const command = args.shift().toLowerCase();
 
-
 //Commands
 var issue;
-
-//Used for testing
-if (command === "test") {
-  cError('Okay Google');
-  cWarn('Okay Google');
-  cPass('Okay Google');
-  uError('Okay Google');
-}
 
 //Help command
 if (command === "help") {
@@ -149,7 +132,7 @@ if (command === "help") {
                description: `**Help For ${client.user.tag}**`,
                fields: [{
                    name: "Chat Commands",
-                   value: 'e'
+                   value: chatCommands.join('\n')
                },
                {
                    name: "Music Commands",
@@ -157,7 +140,7 @@ if (command === "help") {
                },
                {
                    name: "Admin Commands",
-                   value: 'e'
+                   value: adminCommands.join('\n')
                },
                {
                    name: "Bug Reporting",
@@ -196,16 +179,16 @@ if(!reason) reason = "The Kicking Boot Has Speeked!";
  await member.kick(reason)
  .catch(error => message.reply(`${error}`));
  try {
-  if (serverDataa) {
+  if (serverDataMessage) {
    const embed = new Discord.RichEmbed()
      .setTitle("Kick Log")
      .setColor(0xdf2f35)
      .addField("Member Kicked", `${member.user.tag}`, true)
      .addField("Kicked By", `${message.author.tag}`, true)
      .addField("Reason", `${reason}`, true);
-     return message.guild.channels.find("name", serverDataa.get('modlog')).send({ embed });
+     return message.guild.channels.find("name", serverDataMessage.get('modlog')).send({ embed });
    }
- } catch (e) { return message.reply(`Member Kicked. Set up the servrer to have a kick message shown! ${config.prefix}setup`); }
+ } catch (e) { return message.reply(`Member Kicked. Set up the server to have a kick message shown! ${config.prefix}setup`); }
 
 }
 
@@ -224,16 +207,16 @@ if(!reason) reason = "The Ban Hammer Has Spoken!";
  await member.ban(reason)
  .catch(error => message.reply(`${error}`));
  try {
-  if (serverDataa) {
+  if (serverDataMessage) {
    const embed = new Discord.RichEmbed()
      .setTitle("Ban Log")
      .setColor(0xdf2f35)
      .addField("Member Kicked", `${member.user.tag}`, true)
      .addField("Kicked By", `${message.author.tag}`, true)
      .addField("Reason", `${reason}`, true);
-     return message.guild.channels.find("name", serverDataa.get('modlog')).send({ embed });
+     return message.guild.channels.find("name", serverDataMessage.get('modlog')).send({ embed });
    }
- } catch (e) { return message.reply(`Member Banned. Set up the servrer to have a kick message shown! ${config.prefix}setup`); }
+ } catch (e) { return message.reply(`Member Banned. Set up the server to have a ban message shown! ${config.prefix}setup`); }
 
 }
 
@@ -249,22 +232,63 @@ if (command === "coin") {
     }
 }
 
-//Setup Command
+//Server data setup Command
 if (command === "setup") {
     try {
-        const ServerDatab = await ServerData.create({
+        const serverDataSetup = await serverData.create({
             serverid: message.member.guild.id,
             ownerid: message.member.guild.ownerID,
             modlog: `None Set. Go to the modlog channel and type ${config.prefix}modlog`,
             servername: message.member.guild.name,
-            swearfilter: "true",
+            welcomemessage: `Hello! Welcome to [guildname]`,
         });
-        return message.reply(`Server ${tag.servername} added.`);
+        return message.reply(`Server ${serverDataSetup.servername} added.`);
+        cInfo(`New Server Added To Database: ${serverDataSetup.servername}`);
     }
     catch (e) {
-      cError(`Error saving data to database, possible data corruption! Error Name: ${e.name}`);
-      message.reply('Error saving data.');
+        if (e.name === 'SequelizeUniqueConstraintError') {
+            return message.reply(`This server already has data. Run ${config.prefix}data to see it.`);
+        }
+        message.reply('Something went wrong with adding the server data.');
+        return cError(`Error saving data! Error: ${e}`);
     }
+}3
+
+//Server data recalling command
+if (command === "data") {
+  try {
+    if (serverDataMessage) {
+      const embed = new Discord.RichEmbed()
+      .setTitle(`${serverDataMessage.get('servername')} Stored Data`)
+      .setColor(0xff6600)
+      .addField(`Server Name:`, `${serverDataMessage.get('servername')} `)
+      .addField(`Server ID:`, `${serverDataMessage.get('serverid')} `)
+      .addField(`ModLog Channel:`, `#${serverDataMessage.get('modlog')} `)
+      .addField(`Swear Filter Enabled:`, `${serverDataMessage.get('swearfilter')} `)
+      .addField(`Owner ID:`, `${serverDataMessage.get('ownerid')} `);
+      return message.channel.send({embed});
+    }
+  } catch (e) {
+    return message.reply(`Could not find any server info!`);
+  }
+}
+
+//Echo command
+if (command === "echo") {
+  if (!args[0]) return message.reply("No message provided");
+  message.channel.send(args.join(' ') + '˖');
+}
+
+//Clear chat command
+if (command === "ci") {
+  if (!args[0]) return message.reply("Please specify an amount of messages to remove");
+  if (args[0].isNaN) return message.reply("Value provided is not a number");
+  if (args[0] >= 100) return message.reply("Value must be less than 100");
+
+  let messagecount = parseInt(args[0]);
+  message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
+  message.reply(`:recycle: Cleared ${args[0]} messages.`);
+
 }
 
 });

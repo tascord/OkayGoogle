b83b4b9aae71c34473415cf06a74cc3926edcbdd
tascord <35181375+tascord@users.noreply.github.com>
2018-09-29 22:12:03 +1000

    Command updates and additions
    Updated:
    - Kick and Ban reasons will be on a seperate line
    - Kick and Ban now only show usernames not descriminators (they might return in the future)
    - You can now set parts of and delete the servers data
    - You can now choose not to have a welcome message
    - Renamed data command to settings command
    - Welcome messages work
    
    New additions:
    - Invite command (Puts an invite in chat)

--- okaygoogle.js
+++ okaygoogle.js
@@ -15,9 +15,9 @@ moment().tz("Australia/Melbourne").format();
 
 //Variables
 var googleColours = ['0x4681f3', '0xdf2f35', '0xf6c400', '0x477ff5', '0x36bc50', '0xdf2f35'];
-var chatCommands = ['**Coin**\n Flip a coin!', '**Echo**\n Copy whatever you say!'];
+var chatCommands = ['**Coin**\n Flip A Coin!', '**Echo**\n Copy Whatever You Say!'];
 var musicCommands = ["**Play** `[Url / Term]`\n Adds The URL / Search Term To The Queue", "**Skip** `[Number]`\n Skips One Song (or the specified ammount to skip)", "**Queue**\n Shows The Queue", "**Pause / Resume**\n Pauses Or Resumes Music Playback", "**Volume** `[Level]`\n Sets Playback Volume (0 - 200)", "**Leave**\n Removes The Bot From The Voice Channel", "**Clearqueue**\n Clears The Queue"];
-var adminCommands = ['**Setup**\n Setup the servers settings. These can be modified after setup', '**Ban / Kick** `[User] [Reason]`\n Punishes the given user for the given reason', '**Ci** `[Value]`\n Clears the amount of messages you specify'];
+var adminCommands = ['**Setup**\n Setup the servers settings. These can be modified after setup', '**Settings** `[Set / Delete / No Arguement]`\n Set, Delete or Show The Server Settings','**Ban / Kick** `[User] [Reason]`\n Punishes the given user for the given reason', '**Ci** `[Value]`\n Clears the amount of messages you specify', "**Invite**\n Chuck a bot invite in chat"];
 
 //Login the bot
 client.login(config.token);
@@ -80,7 +80,9 @@ client.once('ready', () => {
 client.on("guildMemberAdd", async member => {
 
   try {
-    const guildWelcome = await serverData.findOne({ where: { serverid: message.member.guild.id } });
+    const guildWelcome = await serverData.findOne({ where: { serverid: member.guild.id } });
+    if (guildWelcome.get('welcomemessage').toLowerCase() === 'off') return;
+    client.users.get(member.id).send(guildWelcome.get('welcomemessage'));
   } catch (e) { }
 
 });
@@ -88,20 +90,21 @@ client.on("guildMemberAdd", async member => {
 //Commands
 client.on('message', async message => {
 
-if(!message.member.hasPermission("EMBED_LINKS") || !message.member.hasPermission("MANAGE_MESSAGES")) {
-if(message.content.toLowerCase().includes('www.') || message.content.toLowerCase().includes('http')) {
-message.delete();
-message.reply("Please no URLS in chat");
-}
-}
-
-
+try{
+  if(!message.member.hasPermission("EMBED_LINKS") || !message.member.hasPermission("MANAGE_MESSAGES")) {
+    if(message.content.toLowerCase().includes('www.') || message.content.toLowerCase().includes('http')) {
+      message.delete();
+      message.reply("Please no URLS in chat");
+    }
+  }
+} catch (e) { }
 
 //Variables & Constants
 var now = moment();
 var tmcnt;
 var target;
 var mode;
+var tmp;
 
 //Colour
 var currentColour = googleColours[Math.floor(Math.random() * googleColours.length)];
@@ -163,7 +166,7 @@ if (command === "report"){
 
 //Kick Command
 if (command === "kick") {
-
+serverDataTMP = await serverData.findOne({ where: { serverid: message.member.guild.id } });
 if (!message.member.hasPermission("KICK_MEMBER"))
   return message.reply(`You don't have permission!`);
 let member = message.mentions.members.first();
@@ -180,9 +183,9 @@ if(!reason) reason = "The Kicking Boot Has Speeked!";
    const embed = new Discord.RichEmbed()
      .setTitle("Kick Log")
      .setColor(0xdf2f35)
-     .addField("Member Kicked", `${member.user.tag}`, true)
-     .addField("Kicked By", `${message.author.tag}`, true)
-     .addField("Reason", `${reason}`, true);
+     .addField("Member Kicked", `${member.user.username}`, true)
+     .addField("Kicked By", `${message.author.username}`, true)
+     .addField("Reason", `${reason}`);
      return message.guild.channels.find("name", serverDataTMP.get('modlog')).send({ embed });
    }
  } catch (e) { return message.reply(`Member Kicked. Set up the server to have a kick message shown! ${config.prefix}setup`); }
@@ -191,7 +194,7 @@ if(!reason) reason = "The Kicking Boot Has Speeked!";
 
 //Ban Command
 if (command === "ban") {
-
+serverDataTMP = await serverData.findOne({ where: { serverid: message.member.guild.id } });
 if (!message.member.hasPermission("BAN_MEMBER"))
   return message.reply(`You don't have permission!`);
 let member = message.mentions.members.first();
@@ -208,9 +211,9 @@ if(!reason) reason = "The Ban Hammer Has Spoken!";
    const embed = new Discord.RichEmbed()
      .setTitle("Ban Log")
      .setColor(0xdf2f35)
-     .addField("Member Kicked", `${member.user.tag}`, true)
-     .addField("Kicked By", `${message.author.tag}`, true)
-     .addField("Reason", `${reason}`, true);
+     .addField("Member Kicked", `${member.user.username}`, true)
+     .addField("Kicked By", `${message.author.username}`, true)
+     .addField("Reason", `${reason}`);
      return message.guild.channels.find("name", serverDataTMP.get('modlog')).send({ embed });
    }
  } catch (e) { return message.reply(`Member Banned. Set up the server to have a ban message shown! ${config.prefix}setup`); }
@@ -231,6 +234,7 @@ if (command === "coin") {
 
 //Server data setup Command
 if (command === "setup") {
+  if (args[0] && message.author.id != message.member.guild.ownerID) return message.reply("Only the server owner can create or modify Server Data");
     try {
         const serverDataSetup = await serverData.create({
             serverid: message.member.guild.id,
@@ -244,7 +248,7 @@ if (command === "setup") {
     }
     catch (e) {
         if (e.name === 'SequelizeUniqueConstraintError') {
-            return message.reply(`This server already has data. Run ${config.prefix}data to see it.`);
+            return message.reply(`This server already has data. Run ${config.prefix}settings to see it.`);
         }
         message.reply('Something went wrong with adding the server data.');
     }
@@ -252,35 +256,59 @@ if (command === "setup") {
 
 //Server data recalling and re-setting command command
 if (command === "settings") {
+if (args[0] && message.author.id != message.member.guild.ownerID) return message.reply("Only the server owner can modify Server Data");
 if (!args[0]) {
-  try {
     serverDataTMP = await serverData.findOne({ where: { serverid: message.member.guild.id } });
     if (serverDataTMP) {
+      if (serverDataTMP.get('welcomemessage').toLowerCase() === "off") {
+        tmp = "The user will not receive a welcome message when joining";
+      } else {
+        tmp = serverDataTMP.get('welcomemessage');
+      }
         const embed = new Discord.RichEmbed()
         .setTitle(`${serverDataTMP.get('servername')} Stored Data`)
         .setColor(currentColour)
         .addField(`Server Name:`, `${serverDataTMP.get('servername')} `)
         .addField(`Server ID:`, `${serverDataTMP.get('serverid')} `)
         .addField(`ModLog Channel:`, `#${serverDataTMP.get('modlog')} `)
-        .addField(`Welcome Message:`, `${serverDataTMP.get('welcomemessage')} `)
+        .addField(`Welcome Message:`, tmp)
         .addField(`Owner ID:`, `${serverDataTMP.get('ownerid')} `);
         return message.channel.send({embed});
+      } else {
+        message.reply('There is no data saved for this server.');
       }
-    } catch (e) {
-      message.reply(`Could not find any server info!`);
-    }
-} else if (args[0] === "set") {
+} else if (args[0].toLowerCase() === "set") {
+  if (args[1].toLowerCase() === "modlog") {
+    const affectedRows = await serverData.update({ modlog: message.channel.name }, { where: { serverid: message.member.guild.id } });
+      if (affectedRows > 0) {
+          return message.reply(`Modlog Channel Was Set To: ${message.channel.name}`);
+      }
+      return message.reply(`No Server Data. Setup the server first with ${config.prefix}setup`);
+  } else if (args[1].toLowerCase() === "welcome") {
+    if (!args[2]) return message.reply("You must supply a welcome message or 'Off' to disable the welcome message");
+    const affectedRows = await serverData.update({ welcomemessage: args.slice(2).join(' ') }, { where: { serverid: message.member.guild.id } });
+      if (affectedRows > 0) {
+        if (args.slice(2).join(' ').toLowerCase() === "off") return message.reply("The user will not receive a welcome message after joining.")
+        return message.reply(`Updated Welcome Message.`);
+      }
+      return message.reply(`No Server Data. Setup the server first with ${config.prefix}setup`);
+  } else {
+    return message.reply(`Invalid argument. Data you can set: Modlog Channel (${config.prefix}settings set modlog), Welcome Message (${config.prefix}settings set welcome \`[Off / Welcome Message]\`)`);
+  }
 
+} else if (args[0].toLowerCase() === "delete") {
+  const rowCount = await serverData.destroy({ where: { serverid: message.member.guild.id } });
+  if (!rowCount) return message.reply('This server has no data stored.');
+  message.reply("Server Data Deleted.");
 } else {
   message.reply(`Invalid Command Arguments. E.g \`${config.prefix}settings set modlog\``);
 }
-
 }
 
 //Echo command
 if (command === "echo") {
   if (!args[0]) return message.reply("No message provided");
-  message.channel.send(args.join(' ') + '˖');
+  message.channel.send(`${args.join(' ')}˖`);
 }
 
 //Clear chat command
@@ -292,7 +320,8 @@ if (command === "ci") {
   let messagecount = parseInt(args[0]);
   message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
   message.reply(`:recycle: Cleared ${args[0]} messages.`);
-
 }
+
+if (command === "invite") message.channel.send(`**Invite The Bot Here**\nhttp://bit.ly/OkayGoogleBot`);
 
 });
